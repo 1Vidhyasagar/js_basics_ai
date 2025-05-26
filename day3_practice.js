efba9c7a2bd7625b49e974b435511b652529a248
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2025-05-26 14:45:16 +0530

    Day 3: Arrays, Objects, Scope, Functions, Callbacks - practice done

---
+++ day3_practice.js
@@ -0,0 +1,32 @@
+// Practice:
+
+// Make an array of favorite dishes
+let myFavDishes = ["Cicken Biryani", "Butter Chicken", "Naan"]
+console.log(myFavDishes)
+
+// Make an object for a restaurant menu item
+let menuItem = {
+    name: "Chicken Biryani",
+    Price: 150,
+    isAvailable:true
+};
+console.log(menuItem)
+
+// Write a function prepareOrder(dishName)
+function prepareOrder(type){
+    return `Here is your ${type} meal!`;
+}
+console.log(prepareOrder("Chicken Biryani"))
+
+// Use a callback to say "Enjoy your meal!"
+function greet(name, callback){
+    console.log("Hi " + `${name}`)
+    callback()
+}
+    
+function sayEnjoy(){
+        console.log("Enjoy your meal!")
+    }
+
+
+greet("Vidhyasagar",sayEnjoy)
